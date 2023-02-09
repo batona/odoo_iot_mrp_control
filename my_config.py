950fc7fa4b40be3444298f966d343f882a20e0fe
batona <32574574+batona@users.noreply.github.com>
2023-02-09 18:39:25 +0100

    Create my_config.py

---
+++ my_config.py
@@ -0,0 +1,23 @@
+#wifi credentials
+WLAN_ID = 'wlan_id'
+WLAN_PASS = 'wlan_pass'
+
+#odoo database
+HOST = ''
+PORT = 8069
+DB = ''
+USER = ''
+PASS = ''
+
+#uids of users who will receive notifications
+notification_uids = [1,2,3]
+
+#workcenter
+# wc = 1 #раскрой
+# wc = 2 #пошив корсетов
+# wc = 6 #пошив юбок
+# wc = 7 #соединение
+# wc = 3 #украшение
+# wc = 5 #пришивание
+# wc = 4 #вышивка
+
